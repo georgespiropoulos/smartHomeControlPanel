58028da48d1797c3841f4bde20a9cf83c453e333
Giorgos Spyropoulos <spyrogeo96@gmail.com>
2021-11-24 02:23:32 +0200

    Chore/Minor Changes
    Fixed window size
    window title

--- src/smarthomecontrolpanel/SmartHomeControlPanel.java
+++ src/smarthomecontrolpanel/SmartHomeControlPanel.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+//import javafx.scene.image.Image;
 
 /**
  *
@@ -21,7 +22,10 @@ public class SmartHomeControlPanel extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("smarthome.fxml"));
         
         Scene scene = new Scene(root);
-        
+//        Image icon = new Image(getClass().getResourceAsStream("/resources/img/smart-home.png"));
+//        stage.getIcons().add(icon);
+        stage.setTitle("SmartHouse Control Panel");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
--- src/smarthomecontrolpanel/smarthomeController.java
+++ src/smarthomecontrolpanel/smarthomeController.java
@@ -6,10 +6,10 @@ package smarthomecontrolpanel;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+//import javafx.event.ActionEvent;
+//import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+
 
 /**
  *
@@ -17,14 +17,7 @@ import javafx.scene.control.Label;
  */
 public class smarthomeController implements Initializable {
     
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
