3ce6135211ecd160faa344ba317283640f122256
Giorgos Spyropoulos <spyrogeo96@gmail.com>
2021-12-09 19:07:10 +0200

    feature/header CSS ready

--- src/smarthomecontrolpanel/SmartHomeControlPanel.java
+++ src/smarthomecontrolpanel/SmartHomeControlPanel.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
 
 
 public class SmartHomeControlPanel extends Application {
