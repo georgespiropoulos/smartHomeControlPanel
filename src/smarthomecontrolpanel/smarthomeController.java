5bf1f3e56f6ca69c37508b8cd73993570883bf36
Giorgos Spyropoulos <spyrogeo96@gmail.com>
2021-12-22 18:42:24 +0200

    feature/Date and time has been set

--- src/smarthomecontrolpanel/smarthomeController.java
+++ src/smarthomecontrolpanel/smarthomeController.java
@@ -5,12 +5,19 @@
 package smarthomecontrolpanel;
 
 import java.net.URL;
+import javafx.util.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.ResourceBundle;
-//import javafx.event.ActionEvent;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
 
 
 /**
@@ -19,6 +26,15 @@ import javafx.scene.layout.Pane;
  */
 public class smarthomeController implements Initializable {
     
+    @FXML
+    private Label day;
+    
+    @FXML
+    private Label date;
+    
+    @FXML
+    private Label time;
+        
     @FXML
     private Button homeButton;
     
@@ -94,7 +110,36 @@ public class smarthomeController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        clock();
+        setDate();
+        setDay();
+    }
     
+    private void setDay(){
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE").withLocale(Locale.ENGLISH);
+            day.setText(LocalDateTime.now().format(formatter));
+        }), new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
+    
+    private void setDate(){
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            date.setText(LocalDateTime.now().format(formatter));
+        }), new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
+
+    private void clock() {
+
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+            time.setText(LocalDateTime.now().format(formatter));
+        }), new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
 }
